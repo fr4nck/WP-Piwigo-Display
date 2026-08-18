e34e1489849c6bb3dfd5f2dee32e83393226a391
fr4nck <f.bellardie@gmail.com>
2026-08-19 01:14:17 +0200

    Fix RC bootstrap: restaurer les classes 3.x existantes

--- wp-piwigo-display.php
+++ wp-piwigo-display.php
@@ -33,18 +33,30 @@ if ( ! defined( 'WPD_PLUGIN_URL' ) ) {
 	define( 'WPD_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 }
 
+foreach ( array( 'WPD_Plugin', 'WPD_Settings', 'WPD_Service_Account', 'WPD_Service_Api', 'WPD_Api', 'WPD_Cache', 'WPD_Diagnostic', 'WPD_Renderer', 'WPD_Shortcode', 'WPD_Block', 'WPD_Classic_Editor' ) as $wpd_class ) {
+	if ( class_exists( $wpd_class, false ) ) {
+		return;
+	}
+}
+unset( $wpd_class );
+
+require_once WPD_PLUGIN_DIR . 'includes/class-wpd-plugin.php';
+require_once WPD_PLUGIN_DIR . 'includes/class-wpd-settings.php';
+require_once WPD_PLUGIN_DIR . 'includes/class-wpd-service-account.php';
+require_once WPD_PLUGIN_DIR . 'includes/class-wpd-service-api.php';
 require_once WPD_PLUGIN_DIR . 'includes/class-wpd-api.php';
-require_once WPD_PLUGIN_DIR . 'includes/class-wpd-gallery.php';
-require_once WPD_PLUGIN_DIR . 'includes/class-wpd-admin.php';
+require_once WPD_PLUGIN_DIR . 'includes/class-wpd-cache.php';
+require_once WPD_PLUGIN_DIR . 'includes/class-wpd-diagnostic.php';
+require_once WPD_PLUGIN_DIR . 'includes/class-wpd-renderer.php';
+require_once WPD_PLUGIN_DIR . 'includes/class-wpd-shortcode.php';
 require_once WPD_PLUGIN_DIR . 'includes/class-wpd-block.php';
 require_once WPD_PLUGIN_DIR . 'includes/class-wpd-classic-editor.php';
 
 add_action(
 	'plugins_loaded',
 	static function () {
-		WPD_Gallery::init();
-		WPD_Admin::init();
-		WPD_Block::init();
-		WPD_Classic_Editor::init();
+		WPD_Plugin::init();
+		WPD_Service_Account::register();
+		WPD_Classic_Editor::register();
 	}
 );
